171af1ca54d8f734ce28195a159924a431da37ef
Doni Setiawan <donisetiawan.050@gmail.com>
2020-04-13 19:08:13 +0700

    Styling component

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
@@ -8,13 +8,41 @@ import SimpleButton from './components/SimpleButton';
 import NoteScreen from './components/NoteScreen';
 import HomeScreenx from './components/HomeScreen';
 
+const styles = StyleSheet.create({
+  navBar: {
+    backgroundColor: '#5B29C1',
+    borderBottomColor: '#48209A',
+    borderBottomWidth: 1,
+  },
+  navBarTitleText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: '500',
+    marginVertical: 16,
+  },
+  navBarRightButton: {
+    paddingRight: 10,
+  },
+  navBarButtonText: {
+    color: '#ffffff',
+    fontSize: 16,
+    marginVertical: 16,
+  },
+});
+
 function HomeScreen({ navigation }) {
   navigation.setOptions({
-    headerTitle: () => <Text>React Notes</Text>,
+    headerTitle: () => (
+      <Text style={styles.navBarTitleText}>
+        React Notes
+      </Text>
+    ),
     headerRight: () => (
       <SimpleButton
         onPress={() => navigation.push('Note')}
         customText="Create Note"
+        style={styles.navBarRightButton}
+        textStyle={styles.navBarButtonText}
       />
     ),
   });
@@ -29,7 +57,12 @@ const Stack = createStackNavigator();
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          headerStyle: styles.navBar,
+        }}
+      >
         <Stack.Screen
           name="Home"
           component={HomeScreen}
@@ -38,7 +71,11 @@ function App() {
           name="Note"
           component={NoteScreen}
           options={{
-            headerTitle: () => <Text>Create Note</Text>,
+            headerTitle: () => (
+              <Text style={styles.navBarTitleText}>
+                Create Note
+              </Text>
+            ),
           }}
         />
       </Stack.Navigator>
--- components/NoteScreen.jsx
+++ components/NoteScreen.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component, createRef } from 'react';
 import { TextInput, View, StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
@@ -14,23 +14,52 @@ const styles = StyleSheet.create({
   body: {
     flex: 1,
   },
+  inputContainer: {
+    borderBottomColor: '#9E7CE3',
+    borderBottomWidth: 1,
+    flexDirection: 'row',
+    marginBottom: 10,
+    paddingHorizontal: 20,
+  },
+  textInput: {
+    flex: 1,
+    fontSize: 16,
+  },
 });
 
-function NoteScreen() {
-  return (
-    <View style={styles.container}>
-      <TextInput
-        autoFocus
-        placeholder="Untitled"
-        style={styles.title}
-      />
-      <TextInput
-        multiline
-        placeholder="Start typing"
-        style={styles.body}
-      />
-    </View>
-  );
+class NoteScreen extends Component {
+  constructor(props) {
+    super(props);
+
+    this.bodyRef = createRef();
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.inputContainer}>
+          <TextInput
+            autoFocus
+            placeholder="Untitled"
+            style={[styles.textInput, styles.title]}
+            onEndEditing={() => {
+              this.bodyRef.current.focus();
+            }}
+            underlineColorAndroid="transparent"
+          />
+        </View>
+        <View style={styles.inputContainer}>
+          <TextInput
+            ref={this.bodyRef}
+            multiline
+            placeholder="Start typing"
+            style={[styles.textInput, styles.body]}
+            underlineColorAndroid="transparent"
+          />
+        </View>
+      </View>
+    );
+  }
 }
 
 export default NoteScreen;
